416c176edc43e42f7292165815c3722423f2907e
Evgene <evisotskiy@zaraffasoft.com>
2020-01-28 20:58:27 +0200

    init

---
+++ src/composition/recipes.js
@@ -0,0 +1,19 @@
+import { ref } from '@vue/composition-api';
+
+export function useRecipes() {
+    const recipes = ref([])
+    const current = ref(null)
+
+    const addRecipe = (recipe) => {
+        recipes.value.push(recipe)
+    }
+    const selectRecipe = (id) => {
+        current.value = recipes.value.find(_recipe => _recipe.id === id)
+    }
+    const removeRecipe = (id) => {
+        recipes.value = recipes.value.filter(_recipe => _recipe.id !== id)
+        current.value = null
+    }
+
+    return { recipes, current, addRecipe, selectRecipe, removeRecipe }
+}
---
+++ src/composition/toggle.js
@@ -0,0 +1,8 @@
+import { ref } from '@vue/composition-api'
+
+export function useToggle(initValue = false) {
+    const visible = ref(initValue)
+    const toggle = () => visible.value = !visible.value
+
+    return { visible, toggle }
+}
